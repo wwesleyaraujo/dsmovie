f2b866e65d2d947f063bfa7125e48b9527c9a46c
wwesleyaraujo <wesleyfurnas@gmail.com>
2022-01-14 11:27:42 -0300

    First request

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,7 +1,15 @@
+import axios from "axios";
 import MovieCard from "components/MovieCard";
+import { BASE_URL } from "utils/requests";
 import Pagination from "../../components/Pagination";
 
 function Listing() {
+
+  //FORMA ERRADA 
+    axios.get(`${BASE_URL}/movies`)
+    .then(response =>{
+        console.log(response.data);
+    });
 
     return (
         <div>
